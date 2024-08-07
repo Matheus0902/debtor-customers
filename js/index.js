9fc82c3c872454420aa312ddfc155a6f98d17c21
Matheus <matheus_dev98@hotmail.com>
2024-08-06 21:09:13 -0300

    implementing page routes

---
+++ js/index.js
@@ -0,0 +1,15 @@
+import { Router } from "./router.js"
+
+const router = new Router()
+
+router.add("/", "/pages/home.html")
+router.add("/client", "/pages/client.html")
+router.add("/clients-list", "/pages/clients-list.html")
+router.add("/new-client", "/pages/new-client.html")
+router.add("/register", "/pages/register.html")
+router.add(404, "/pages/404.html")
+
+router.handle()
+
+window.onpopstate = () => router.handle()
+window.route = () => router.route()

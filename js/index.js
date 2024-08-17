1883a004d536ab12e5a53a66337d91f9665c6e21
Matheus <matheus_dev98@hotmail.com>
2024-08-17 12:19:47 -0300

    refreshing client-account

--- js/index.js
+++ js/index.js
@@ -3,7 +3,7 @@ import { Router } from "./router.js"
 const router = new Router()
 
 router.add("/", "/pages/home.html")
-router.add("/client", "/pages/client.html")
+router.add("/client-account", "/pages/client-account.html")
 router.add("/clients-list", "/pages/clients-list.html")
 router.add("/new-client", "/pages/new-client.html")
 router.add("/new-entry", "/pages/new-entry.html")
